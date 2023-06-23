8438f6511cac1af59dc6f9054e4dd6f67806bfbe
sr.bala <sr.bala.kkl@gmail.com>
2023-06-23 08:22:32 +0530

    new:paneComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {PaneComponent} from "./pane/pane.component";
 import {ChildComponent} from "./child/child.component";
 
@@ -7,8 +7,8 @@ import {ChildComponent} from "./child/child.component";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements AfterViewInit{
-  title = 'untitled4';
+export class AppComponent implements OnInit, AfterViewInit{
+  title = 'ViewChildDecorator';
   @ViewChild(PaneComponent)
   set pane(v: PaneComponent) {
     setTimeout(() => {
@@ -25,22 +25,30 @@ export class AppComponent implements AfterViewInit{
   }
 
 //   ****** Accessing HTML Element using @ViewChild Decorator ******
-  @ViewChild('headline') headlinevalue!:ElementRef;
+//   @ViewChild('headline') headlinevalue!:ElementRef;
   /* Since we are accessing html element,
     I'm declaring its type as ElementRef. */
 
   @ViewChild(ChildComponent) getchildVariable!:ChildComponent;
   //here I'm referencing the ChildComponent to the CurrentComponent
+  @ViewChildren(ChildComponent) getViewChildren!:QueryList<ChildComponent>
+
+  ngOnInit() {
+    // note: the below will give error
+    // console.log(`Calling from ngOnInit = ${this.getchildVariable.childVariable}`)
+  }
 
   ngAfterViewInit() {
     /*note: this will only available only after the view is rendered / initialized in the DOM
     * So this will only work on ngAfterViewInit() lifecycle hook NOT on ngOnInit. */
-
     /*By default, if u use it on ngOnInit it'll show undefined error*/
-    console.log(`The Element Ref Value == ${this.headlinevalue.nativeElement.innerHTML}`)
+
     this.getchildVariable.childVariable = 'Changed Variable';
     console.log(this.getchildVariable.childVariable)
-
+    console.log(this.getchildVariable.myname)
+    console.log(`Getting value by using the  @ViewChildren is ${this.getViewChildren.length}`)
+    console.log('get view children =',this.getViewChildren.get(0)?.childVariable)
   }
 
 }
+
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ChildComponent implements OnInit {
 
   childVariable:string = 'Initial Value';
+  childVariable2:string = 'Initial Value 2'
+  myname:string = 'bala';
+
   constructor() { }
 
   ngOnInit(): void {
